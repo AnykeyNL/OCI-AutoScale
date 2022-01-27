abeaab35f2e1ca45d63be483880e3ecd1f8a7e7f
Richard Garsthagen <the.anykey@gmail.com>
2022-01-27 14:19:29 +0100

    Added support for Data Integration Workspaces

--- AutoScaleALL.py
+++ AutoScaleALL.py
@@ -516,7 +516,11 @@ def autoscale_region(region):
     query += " && compartmentId != '" + compartment_exclude + "'" if compartment_exclude else ""
     sdetails = oci.resource_search.models.StructuredSearchDetails()
     sdetails.query = query
-    result = search.search_resources(search_details=sdetails, limit=1000, retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY).data
+    try:
+        result = search.search_resources(search_details=sdetails, limit=1000, retry_strategy=oci.retry.DEFAULT_RETRY_STRATEGY).data
+    except oci.exceptions.ServiceError as response:
+        print ("Error: {} - {}".format(response.code, response.message))
+        result = []
 
     #################################################################
     # Find additional resources not found by search (MySQL Service)
