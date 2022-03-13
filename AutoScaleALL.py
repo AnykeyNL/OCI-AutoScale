1fba4cf9eb0908ed9fa490b790b85c603144f9e8
Richard Garsthagen <the.anykey@gmail.com>
2022-03-13 14:25:12 +0100

    New DayOfMonth tag supported. Hopefully fixed timezone issues and created seperate file to maintain regions and their timezones.
    Fixed the CreateNameSpaces script to support all authentication methods

--- AutoScaleALL.py
+++ AutoScaleALL.py
@@ -34,6 +34,7 @@ import sys
 import argparse
 import os
 import Regions
+import OCIFunctions
 
 # You can modify / translate the tag names used by this script - case sensitive!!!
 AnyDay = "AnyDay"
@@ -100,74 +101,6 @@ def get_current_hour(region, ignore_region_time=False):
     return iDayOfWeek, iDay, iCurrentHour, iDayOfMonth
 
 
-##########################################################################
-# Create signer for Authentication
-# Input - config_profile and is_instance_principals and is_delegation_token
-# Output - config and signer objects
-##########################################################################
-def create_signer(config_profile, is_instance_principals, is_delegation_token):
-
-    # if instance principals authentications
-    if is_instance_principals:
-        try:
-            signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
-            config = {'region': signer.region, 'tenancy': signer.tenancy_id}
-            return config, signer
-
-        except Exception:
-            print_header("Error obtaining instance principals certificate, aborting")
-            raise SystemExit
-
-    # -----------------------------
-    # Delegation Token
-    # -----------------------------
-    elif is_delegation_token:
-
-        try:
-            # check if env variables OCI_CONFIG_FILE, OCI_CONFIG_PROFILE exist and use them
-            env_config_file = os.environ.get('OCI_CONFIG_FILE')
-            env_config_section = os.environ.get('OCI_CONFIG_PROFILE')
-
-            # check if file exist
-            if env_config_file is None or env_config_section is None:
-                MakeLog("*** OCI_CONFIG_FILE and OCI_CONFIG_PROFILE env variables not found, abort. ***")
-                MakeLog("")
-                raise SystemExit
-
-            config = oci.config.from_file(env_config_file, env_config_section)
-            delegation_token_location = config["delegation_token_file"]
-
-            with open(delegation_token_location, 'r') as delegation_token_file:
-                delegation_token = delegation_token_file.read().strip()
-                # get signer from delegation token
-                signer = oci.auth.signers.InstancePrincipalsDelegationTokenSigner(delegation_token=delegation_token)
-
-                return config, signer
-
-        except KeyError:
-            MakeLog("* Key Error obtaining delegation_token_file")
-            raise SystemExit
-
-        except Exception:
-            raise
-
-    # -----------------------------
-    # config file authentication
-    # -----------------------------
-    else:
-        config = oci.config.from_file(
-            oci.config.DEFAULT_LOCATION,
-            (config_profile if config_profile else oci.config.DEFAULT_PROFILE)
-        )
-        signer = oci.signer.Signer(
-            tenancy=config["tenancy"],
-            user=config["user"],
-            fingerprint=config["fingerprint"],
-            private_key_file_location=config.get("key_file"),
-            pass_phrase=oci.config.get_config_value_or_default(config, "pass_phrase"),
-            private_key_content=config.get("key_content")
-        )
-        return config, signer
 
 
 ##########################################################################
@@ -1567,7 +1500,7 @@ start_time = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 print_header("Running Auto Scale")
 
 # Identity extract compartments
-config, signer = create_signer(cmd.config_profile, cmd.is_instance_principals, cmd.is_delegation_token)
+config, signer = OCIFunctions.create_signer(cmd.config_profile, cmd.is_instance_principals, cmd.is_delegation_token)
 compartments = []
 tenancy = None
 tenancy_home_region = ""
---
+++ OCIFunctions.py
@@ -0,0 +1,70 @@
+import oci
+
+##########################################################################
+# Create signer for Authentication
+# Input - config_profile and is_instance_principals and is_delegation_token
+# Output - config and signer objects
+##########################################################################
+def create_signer(config_profile, is_instance_principals, is_delegation_token):
+
+    # if instance principals authentications
+    if is_instance_principals:
+        try:
+            signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
+            config = {'region': signer.region, 'tenancy': signer.tenancy_id}
+            return config, signer
+
+        except Exception:
+            print_header("Error obtaining instance principals certificate, aborting")
+            raise SystemExit
+
+    # -----------------------------
+    # Delegation Token
+    # -----------------------------
+    elif is_delegation_token:
+
+        try:
+            # check if env variables OCI_CONFIG_FILE, OCI_CONFIG_PROFILE exist and use them
+            env_config_file = os.environ.get('OCI_CONFIG_FILE')
+            env_config_section = os.environ.get('OCI_CONFIG_PROFILE')
+
+            # check if file exist
+            if env_config_file is None or env_config_section is None:
+                MakeLog("*** OCI_CONFIG_FILE and OCI_CONFIG_PROFILE env variables not found, abort. ***")
+                MakeLog("")
+                raise SystemExit
+
+            config = oci.config.from_file(env_config_file, env_config_section)
+            delegation_token_location = config["delegation_token_file"]
+
+            with open(delegation_token_location, 'r') as delegation_token_file:
+                delegation_token = delegation_token_file.read().strip()
+                # get signer from delegation token
+                signer = oci.auth.signers.InstancePrincipalsDelegationTokenSigner(delegation_token=delegation_token)
+
+                return config, signer
+
+        except KeyError:
+            MakeLog("* Key Error obtaining delegation_token_file")
+            raise SystemExit
+
+        except Exception:
+            raise
+
+    # -----------------------------
+    # config file authentication
+    # -----------------------------
+    else:
+        config = oci.config.from_file(
+            oci.config.DEFAULT_LOCATION,
+            (config_profile if config_profile else oci.config.DEFAULT_PROFILE)
+        )
+        signer = oci.signer.Signer(
+            tenancy=config["tenancy"],
+            user=config["user"],
+            fingerprint=config["fingerprint"],
+            private_key_file_location=config.get("key_file"),
+            pass_phrase=oci.config.get_config_value_or_default(config, "pass_phrase"),
+            private_key_content=config.get("key_content")
+        )
+        return config, signer
